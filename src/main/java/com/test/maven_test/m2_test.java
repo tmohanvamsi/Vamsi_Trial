06b597c4ed27566b881337d2d1ecf1831cb4f653
tmohanvamsi <tmohanvamsi@gmail.com>
2017-04-18 23:36:52 -0400

    changed

--- src/main/java/com/test/maven_test/m2_test.java
+++ src/main/java/com/test/maven_test/m2_test.java
@@ -2,6 +2,6 @@ package com.test.maven_test;
 
 public class m2_test {
 	public static void main(String[] args){
-		System.out.println("Hi ! The jar is built");
+		System.out.println("Hi ! you can see the screen");
 	}
 }

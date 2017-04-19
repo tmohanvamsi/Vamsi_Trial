dbb7744488ea66d567210420045c1d669dbd52dd
tmohanvamsi <tmohanvamsi@gmail.com>
2017-04-18 23:47:17 -0400

    latest

--- src/main/java/com/test/maven_test/m2_test.java
+++ src/main/java/com/test/maven_test/m2_test.java
@@ -2,6 +2,6 @@ package com.test.maven_test;
 
 public class m2_test {
 	public static void main(String[] args){
-		System.out.println("Hi ! you can see the screen");
+		System.out.println("Hi ! Its Successfull");
 	}
 }
